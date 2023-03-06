51774468fa3e53e870c7865f1b8eb5d231d6296e
Masya1 <i.am.masya1@gmail.com>
2023-03-06 10:58:01 +0200

    Started ORM setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PostgresConfigService } from './database/config/postgres-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: PostgresConfigService,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/database/config/postgres-config.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): PostgresConnectionOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.getOrThrow('PSQL_HOST'),
+      port: +this.configService.getOrThrow('PSQL_PORT'),
+      username: this.configService.getOrThrow('PSQL_USERNAME'),
+      password: this.configService.getOrThrow('PSQL_PASSWORD'),
+      database: this.configService.getOrThrow('PSQL_DATABASE'),
+      entities: ['dist/database/entities/*.js'],
+      synchronize: true,
+      // migrations: ['dist/src/db/migrations/*.js'],
+    };
+  }
+}
